67518a42c796832c12984fcdaad271771e5f7160
真是刘皇叔 <luke.bei.2015@gmial.com>
2023-02-15 17:52:19 +0800

    🗃️  Setup model

--- signup_api/models.py
+++ signup_api/models.py
@@ -1,3 +1,18 @@
+import uuid
 from django.db import models
 
+
 # Create your models here.
+class Company(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    company_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "company"
+
+        def __str__(self) -> str:
+            return self.company_name
